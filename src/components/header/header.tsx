597851090759e3418c423003d8536615865004a7
Pamela <pamelacantaruti@hotmail.com>
2025-05-27 21:13:30 -0300

    feat: created header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,60 @@
+"use client";
+import styled from "styled-components";
+
+const TagHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 18px 160px;
+  background: var(--color-medium-gray);
+  text-transform: uppercase;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+  }
+`;
+const Logo = styled.a`
+  color: var(--color-soft-white);
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 150%;
+  cursor: pointer;
+  &:hover {
+    color: var(--color-dark-gray);
+  }
+`;
+
+const NavContent = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+
+  li {
+    color: var(--color-soft-white);
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 150%;
+    cursor: pointer;
+    list-style: none;
+    &:hover {
+      color: var(--color-dark-gray);
+    }
+  }
+`;
+
+export function Header() {
+  return (
+    <TagHeader>
+      <Logo>&lt;Pamela Cantaruti/&gt;</Logo>
+      <NavContent>
+        <li>Sobre</li>
+        <li>Habilidades</li>
+        <li>Projetos</li>
+      </NavContent>
+    </TagHeader>
+  );
+}
