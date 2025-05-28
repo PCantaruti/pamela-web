774f787d24189fe16c027af264f90ae8fe1588e3
Pamela <pamelacantaruti@hotmail.com>
2025-05-27 22:39:27 -0300

    feat: [wip] intro page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { AboutIntro } from "@/components/AboutIntro/AboutIntro";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Hello Devs</main>;
+  return (
+    <main className={styles.main}>
+      <AboutIntro />
+    </main>
+  );
 }
